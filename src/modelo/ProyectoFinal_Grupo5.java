50bd2ddeda8d34a07e1911bf4dc5fe9861393e7a
gabrielaballerini-sketch <gabrielaballerini@gmail.com>
2025-10-18 17:13:31 -0300

    Inicio Clases

---
+++ src/modelo/ProyectoFinal_Grupo5.java
@@ -0,0 +1,17 @@
+
+package modelo;
+
+
+public class ProyectoFinal_Grupo5 {
+
+    public static void main(String[] args) {
+        
+       Conexion.getconexion();
+       
+       
+        
+        
+     
+    }
+    
+}
